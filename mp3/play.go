fe544591e08be25fc0f7842573efa799be87a26e
maxwell92 <710028463@qq.com>
2016-07-24 20:35:50 +0800

    too many errors.

---
+++ mp3/play.go
@@ -0,0 +1,23 @@
+//package mp
+package main
+import "fmt"
+
+type Player interface {
+    Play(source string)
+}
+
+func Play(source, mtype string ) {
+    var p player
+
+    switch mtype {
+        case "MP3":
+            p = &MP3Player{}
+        case "WAV":
+            p = &WAVPlayer{}
+        default:
+            fmt.Println("Unsupported music type", mtype)
+            return
+    }
+
+    p.Play(source)
+}
