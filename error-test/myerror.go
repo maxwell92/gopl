bb6264c48ed0aaf42873fcb303539ee3a2b96c8f
maxwell92 <710028463@qq.com>
2016-08-22 22:02:00 +0800

    add error basic test

--- error-test/myerror.go
+++ error-test/myerror.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	//	"log"
 )
 
 type myerror interface {
@@ -20,15 +21,44 @@ func (e *errorString) Error() string {
 	return e.text
 }
 
+// different method with interface doesn't work
+/*
+func (e errorString) Errornormal() string {
+	return e.text
+}
+
+func (e *errorString) Errorpointer() string {
+	return e.text
+}
+*/
 func main() {
+	/*
+		log.Printf("%v\n", New("this is error").Error())
+		log.Println(New("this is error").Error())
 
-	log.Printf("%v\n", New("this is error").Error())
-	log.Println(New("this is error").Error())
+		myerr := New("myerror is error")
+		errmsg := myerr.Error()
+		log.Println(errmsg)
 
-	myerr := New("myerror is error")
-	errmsg := myerr.Error()
-	log.Println(errmsg)
+		errmsg2 := New("myerror is error2").Error()
+		log.Println(errmsg2)
 
-	errmsg2 := New("myerror is error2").Error()
-	log.Println(errmsg2)
+		fmt.Println(New("EOF") == New("EOF"))
+	*/
+
+	// no matter it's pointer receiver for normal receiver,
+	// no same error text address
+	/*
+		fmt.Println("pointer")
+		for i := 0; i < 5; i++ {
+			msg := New("EOF").Error()
+			fmt.Printf("%p\n", &msg)
+		}
+
+		fmt.Println("normal")
+		for i := 0; i < 5; i++ {
+			msg := New("EOF").Error()
+			fmt.Printf("%p\n", &msg)
+		}
+	*/
 }
