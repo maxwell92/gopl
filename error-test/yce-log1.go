3da47aa56e847d325f94f70ca7c18c0876bbbd9d
maxwell92 <710028463@qq.com>
2016-08-23 17:30:31 +0800

    yce log test

---
+++ error-test/yce-log1.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+)
+
+type LogLevel uintptr
+
+const (
+	FATAL LogLevel = 1
+	ERROR LogLevel = 2
+	WARN  LogLevel = 3
+	INFO  LogLevel = 4
+	DEBUG LogLevel = 5
+	TRACE LogLevel = 6
+)
+
+var loglevels = [...]string{
+	FATAL: "FATAL",
+	ERROR: "ERROR",
+	WARN:  "WARN",
+	INFO:  "INFO",
+	DEBUG: "DEBUG",
+	TRACE: "TRACE",
+}
+
+type YceLogger struct {
+	Logger *log.Logger
+}
+
+func New() *YceLogger {
+	return &YceLogger{
+		Logger: log.New(os.Stderr, "", log.Lshortfile),
+	}
+}
+
+func (l LogLevel) Log() string {
+	return loglevels[l]
+}
+
+func (y *YceLogger) Printf(level LogLevel, funcName, context string) {
+	y.Logger.SetPrefix(fmt.Sprintf("%s [%s] ", time.Now().Format("2006-01-02 15:04:05"), level.Log()))
+	y.Logger.Printf("%s: %s\n", funcName, context)
+}
+
+func (y *YceLogger) Fatalf(level LogLevel, funcName, context string) {
+	y.Logger.SetPrefix(fmt.Sprintf("%s [%s] ", time.Now().Format("2006-01-02 15:04:05"), level.Log()))
+	y.Logger.Fatalf("%s: %s\n", funcName, context)
+
+}
+
+func main() {
+	y := New()
+	y.Printf(DEBUG, "main()", "main debug log")
+	y.Fatalf(FATAL, "main()", "main fatal error")
+}
