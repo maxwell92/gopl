8ad55c496cd4c0bd10b03719b5df72de39fd7586
maxwell92 <710028463@qq.com>
2016-09-16 16:06:36 +0800

    add substring test

---
+++ string/substring.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	//"string"
+)
+
+func main() {
+	str := "Hello World"
+	l := len(str)
+	fmt.Printf("str[%d]: %v\n", 0, str[0])
+	fmt.Printf("str[%d]: %v\n", l-1, str[l-1])
+
+	fmt.Printf("str[%d:%d]: %s\n", 0, 5, str[0:5])
+}
