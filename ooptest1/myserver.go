a531c772407e0b7d172dc9cfe2acbd156c94564b
maxwell92 <710028463@qq.com>
2016-09-18 10:55:19 +0800

    add test of multi inherit

---
+++ ooptest1/myserver.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"github.com/kataras/iris"
+)
+
+type showController struct {
+	baseController
+	msgController
+}
+
+type baseController struct {
+	*iris.Context
+}
+
+type msgController struct {
+	msg string
+}
+
+func (s showController) Get() {
+	fmt.Println(s.msg) //TODO: why s.msg is "" ???
+	//s.Write(s.msg)
+}
+
+func main() {
+	myshow := new(showController)
+	myshow.msg = "Hello World"
+
+	fmt.Println(myshow.msg)
+	iris.API("/msg", *myshow) //TODO: how to reflect ??
+	iris.Listen(":9999")
+}
