b89236ec4ef00d16ce3cedc32b573277f6edb2ac
maxwell92 <710028463@qq.com>
2016-09-29 10:28:54 +0800

    updated

--- nil/nil.go
+++ nil/nil.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 	"unsafe"
 )
 
@@ -13,6 +14,7 @@ func nilget() *test {
 	return nil
 }
 
+// better not pass pointer nil to interface{}
 func checkNotNil(value interface{}) bool {
 	v := unsafe.Pointer(&value)
 	if v == nil {
@@ -22,10 +24,18 @@ func checkNotNil(value interface{}) bool {
 		fmt.Printf("not nil: %v\n", v)
 		return true
 	}
+}
 
+func checkValidate(value interface{}) bool {
+	if reflect.TypeOf(value).Kind() == reflect.Ptr && value != nil {
+		return true
+	} else {
+		return false
+	}
 }
 
 func main() {
 	value := nilget()
-	fmt.Printf("%v\n", checkNotNil(value))
+	//fmt.Printf("%v\n", checkNotNil(value))
+	fmt.Printf("%v\n", checkValidate(value))
 }
