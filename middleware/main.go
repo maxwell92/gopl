6ab5b2dbad449e1c6d111467aba64c1adbd0aff7
maxwell92 <710028463@qq.com>
2016-11-30 14:51:19 +0800

    add middleware test

---
+++ middleware/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"github.com/kataras/iris"
+)
+
+type Car interface {
+	Show() string
+}
+
+type Benz struct {
+	Brand string
+	*iris.Context
+}
+
+type Bmw struct {
+	Price string
+	*iris.Context
+}
+
+func (b Benz) Show() string {
+	return b.Brand
+}
+
+func (b Bmw) Show() string {
+	return b.Price
+}
+
+func (b Bmw) Get() {
+	b.Write("This is bmw")
+	fmt.Println("bmw get")
+}
+
+func (b *Bmw) BmwInfo(ctx *iris.Context) {
+	ctx.Write(b.Show())
+}
+
+func (b Benz) Get() {
+	b.Write("This is Benz")
+	fmt.Println("benz get")
+}
+
+func (b *Benz) BenzInfo(ctx *iris.Context) {
+	ctx.Write(b.Show())
+	ctx.Next()
+}
+
+func Hi(ctx *iris.Context) {
+	fmt.Println("This 2nd middleware")
+	ctx.Write("hi")
+}
+
+func Display(ctx *iris.Context, c Car) {
+	c.Show()
+}
+
+func main() {
+	bmw := &Bmw{
+		Price: "$20,000",
+	}
+	benz := &Benz{
+		Brand: "Mercedes Benz",
+	}
+	//iris.API("/bmw", *bmw, bmw.BmwInfo)
+	iris.API("/bmw", *bmw)
+	//iris.API("/benz", *benz, Display)
+	//iris.API("/benz", *benz, benz.BenzInfo) // This is benz. benz get.
+	//iris.API("/benz", *benz, benz.BenzInfo, Hi) // This is benz.Hi.
+	iris.Listen(":8081")
+}
