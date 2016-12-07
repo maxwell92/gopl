aef3f9612664db7af500b6697e709bde3fa5314e
maxwell92 <710028463@qq.com>
2016-12-07 15:59:37 +0800

    add struct test

--- struct/struct.go
+++ struct/struct.go
@@ -10,14 +10,23 @@ type struct1 struct {
 	member []struct2
 }
 
-func main() {
-	n := &struct1{
-		member: []struct2{
-			struct2{
-				number: []int32{1, 2, 3},
-			},
-		},
-	}
+type struct3 struct {
+	member *struct2
+}
 
-	fmt.Printf("%v\n", n)
+func main() {
+	/*
+		n := &struct1{
+			member: []struct2{
+				struct2{
+					number: []int32{1, 2, 3},
+				},
+			},
+		}
+
+		fmt.Printf("%v\n", n)
+	*/
+	s3 := new(struct3)
+	fmt.Println("s3: %v\ts3.member: %v\n", &s3, &s3.member)
+	fmt.Println("s3: %v\ts3.member: %v\n", s3, s3.member)
 }
