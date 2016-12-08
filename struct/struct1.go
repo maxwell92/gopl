88a877ac986d508a05a83746bea0594450f5f987
maxwell92 <710028463@qq.com>
2016-12-08 16:01:04 +0800

    add omitempty json tag test

--- struct/struct1.go
+++ struct/struct1.go
@@ -7,15 +7,15 @@ import (
 
 type Car struct {
 	Name   string `json:"name"`
-	Booked bool   `json:"Booked"`
-	Sold   bool   `json:"sold"`
+	Booked bool   `json:"Booked,omitempty"`
+	Sold   bool   `json:"sold,omitempty"`
 }
 
 func main() {
 	Benz := &Car{
 		Name:   "奔驰",
 		Booked: true,
-		Sold:   false,
+		Sold:   false, //添加了omitempty字段后，没值或者赋为该字段0值都不会对它进行json编码
 	}
 
 	info, _ := json.Marshal(Benz)
