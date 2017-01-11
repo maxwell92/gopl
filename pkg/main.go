eb9a4bafada2b078f1d195b608f8bfe1df20d9ab
maxwell92 <710028463@qq.com>
2017-01-11 22:37:46 +0800

    add custom src pkg test

---
+++ pkg/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"str"
+	mi "tools/integer"
+)
+
+func main() {
+	str.Hello = "HelloWorld"
+	fmt.Println(str.Hello)
+
+	mi.Number = 10
+	fmt.Println(mi.Number)
+}
---
+++ pkg/src/tools/integer/integer.go
@@ -0,0 +1,3 @@
+package integer
+
+var Number int
