d0646030ae177592d2df4ea2105d4a8a4c3a232a
maxwell92 <710028463@qq.com>
2017-02-07 21:18:38 +0800

    update types

--- sort/deployment-test/types.go
+++ sort/deployment-test/types.go
@@ -16,7 +16,15 @@ type ListDeploymentController struct {
 	UpdateTime     func(sorter.IResource, sorter.IResource) bool
 }
 
+/*
 func (ldc *ListDeploymentController) OrderedBy(less ...sorter.LessFunc) *sorter.Sorter {
+	return &sorter.Sorter{
+		Lessf: less,
+	}
+}
+*/
+
+func (c *Controller) OrderedBy(less ...sorter.LessFunc) *sorter.Sorter {
 	return &sorter.Sorter{
 		Lessf: less,
 	}
