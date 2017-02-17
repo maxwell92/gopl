b6a659990beb46c6fb56dc53a4bc5799efd9cdae
maxwell92 <710028463@qq.com>
2017-02-17 11:00:51 +0800

    add print test

--- slice/print.go
+++ slice/print.go
@@ -3,10 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	b := []int{1, 2, 3}
+	b := []int{1, 2, 3, 4, 5}
 	fmt.Println(b)
 
-	l := b[1:3]
+	page := 1
+	start := page - 1
+	offset := 5
+
+	a := b[start : start+offset]
+	fmt.Println(a)
+
+	l := b[0:3]
 	fmt.Println(l)
 
 	l = b[1:]
