d7c8c0d080b016f0e5f223aca58bdeaee50a0da8
mlycore <maxwell92@126.com>
2017-10-18 16:33:47 +0800

    两次写回测试“

--- httprouter/demo1.go
+++ httprouter/demo1.go
@@ -4,12 +4,16 @@ import (
 	"fmt"
 	"github.com/julienschmidt/httprouter"
 	"net/http"
+	"time"
 )
 
 func main() {
 	router := httprouter.New()
 	router.GET("/hello", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params){
-		fmt.Fprintf(w, "Hello World\n")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		fmt.Fprintf(w, "Hello\n")
+		time.Sleep(5 * time.Second)
+		fmt.Fprintf(w, "World\n")
 	})
 	http.ListenAndServe(":8082", router)
 }
