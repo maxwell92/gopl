6c33b1f78300e41c0a1c248412b9f90fc4c3f13a
mlycore <maxwell92@126.com>
2018-01-15 10:51:40 +0800

    add demo

---
+++ mastering-go-web-services/demo1.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+type API struct{
+	Message string `json:"message"`
+}
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	urlParams := mux.Vars(r)
+	name := urlParams["user"]	
+	HelloMessage := "Hello " + name
+
+	message := API{HelloMessage}
+	output, _ := json.Marshal(message)
+	fmt.Fprintf(w, "%s", string(output))
+}
+
+func main() {
+	gorillaRoute := mux.NewRouter()	
+	gorillaRoute.HandleFunc("/api/{user:[0-9]+}", Hello)
+	http.Handle("/", gorillaRoute)
+	http.ListenAndServe(":8080", nil)
+}
